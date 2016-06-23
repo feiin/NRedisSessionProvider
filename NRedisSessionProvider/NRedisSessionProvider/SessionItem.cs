b93ea6564c80f7407bf0d9cfeb1d879582504884
feiin <feiin@qq.com>
2016-06-23 11:08:49 +0800

    add sessionitem

---
+++ NRedisSessionProvider/NRedisSessionProvider/SessionItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.SessionState;
+
+namespace NRedisSessionProvider
+{
+	public class SessionItem
+	{
+		public SessionItem()
+		{
+		}
+
+		public DateTime CreatedAt { get; set; }
+		public DateTime LockAt { get; set; }
+		public int LockID { get; set; }
+		public int Timeout { get; set; }
+		public bool Locked { get; set; }
+		public ISessionStateItemCollection SessionItems { get; set; }
+		public int Flags { get; set; }
+	}
+}
+
